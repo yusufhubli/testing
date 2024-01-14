a11b554f03020bb9959ba96275aec38064279daa
yusufhubli <yusufhubli7@gmail.com>
2024-01-14 15:34:32 +0530

    added public folder2

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 // const exp = require("constants")
 // const express = require("express")
 // const path = require("path")
-
+console.log(path.dirname('csstest/public'))
 const app = express()
 app.use(express.static('./public'))
 app.use(express.static('./views'))
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve('./views/index.html'))
+    res.sendFile(path.resolve('./css/index.css'))
 })
 app.listen(5000,()=>console.log('listen at 5000'))
