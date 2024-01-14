980891959ce9fa26ea3849535470befc7d47c588
yusufhubli <yusufhubli7@gmail.com>
2024-01-14 14:34:45 +0530

    addes static css and js files

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import path from 'path'
 const app = express()
 
 app.use(express.static('./views'))
+app.use(express.static('./views/css'))
+app.use(express.static('./views/js'))
 
 app.get('/home',(req,res)=>{
     res.sendFile(path.resolve('./views/index.html'))
